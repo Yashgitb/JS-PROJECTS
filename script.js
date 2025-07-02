e0ca8b488aae5b80ced72837d363c4f56c217702
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-02 23:26:07 +0530

    Updated script.js without % function

--- script.js
+++ script.js
@@ -14,11 +14,29 @@ buttons.forEach(button => {
     else if(value === 'AC' ){
       string = "";
     }
-     else if(value === 'DL' ){
+    else if(value === 'DL' ){
+      string = string.slice(0,-1);
+    }
+    else if(value === 'X' ){
       string = string.slice(0,-1);
     }
     else if(value === '%' ){
        string = (eval(string))/100;
+    }
+    else if(value === 'x^2' ){
+       let square = Number(string);
+       square = square**2;
+       string=String(square);
+    }
+    else if(value === "1/x"){
+      let inver = Number(string);
+       inver = 1/inver;
+       string = String(inver);
+    }
+    else if (value === "x^0.5"){
+      let squareroot = Number(string);
+       squareroot = Math.sqrt(squareroot);
+       string = String(squareroot);
     }
     else {
         string = string+value;        
