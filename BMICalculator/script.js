0ac80459e49c499e5fe793e290b939e029720eb6
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-08-06 16:22:06 +0530

    Update script.js

--- BMICalculator/script.js
+++ BMICalculator/script.js
@@ -1,30 +1,30 @@
+const form = document.querySelector('form');
+//this usecase will givr you empty
+//const height = parseINt(document.querySelector('#height').value)
 
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector("body");
-
-buttons.forEach(function (button) {
-
-    console.log(button);
-    button.addEventListener('click',function (e){
-    
-      console.log(e);
-      console.log(e.target);
-      if (e.target.id === 'grey'){
-        body.style.backgroundColor = e.target.id 
-      }
-      if (e.target.id === 'white'){
-        body.style.backgroundColor = e.target.id 
-      }
-      if (e.target.id === 'blue'){
-        body.style.backgroundColor = e.target.id 
-      }
-      if (e.target.id === 'yellow'){
-        body.style.backgroundColor = e.target.id 
-      }
+    form.addEventListener('submit' , function (e) {
+      e.preventDefault();
       
-    })
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
 
-})
+    if (height === ' ' ||  height < 0 || isNaN(height)){
+
+        results.innerHTML = `Please give a valid height ${height}`;
+
+    }
+    if (weight === ' ' ||  weight < 0 || isNaN(weight)){
+
+        results.innerHTML = `Please give a valid height ${weight}`;
+
+    }
+
+    let BMI = weight/(height*height);
+    BMI = BMI*10000;
+    results.innerHTML = `The BMI of the person is ${BMI}`
+    
 
 
+  });
 
